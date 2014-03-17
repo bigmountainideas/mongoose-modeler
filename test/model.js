e2cf179f7abad60974242942dd170542565c4013
Jovan Alleyne <jovan.alleyne@gmail.com>
2014-03-16 23:53:51 -0400

    Added test case for modeler.connection mothod

--- test/model.js
+++ test/model.js
@@ -3,43 +3,43 @@ var modeler = require('../')
 ;
 
 describe('Modeler', function(){
-  
+
   var animals_db, objects_db;
-  
+
   beforeEach(function(done){
-    
+
     var db1 = mongoose.createConnection('mongodb://localhost/animals')
       , db2 = mongoose.createConnection('mongodb://localhost/objects')
     ;
-    
+
     db1.once('open', function(){
       animals_db = this;
       if( objects_db) done()
     });
-    
+
     db2.once('open', function(){
       objects_db = this;
       if( animals_db) done()
     });
-    
+
   })
   afterEach(function(done){
     animals_db = objects_db = null; done();
   })
-  
+
   it('should create istance of Modeler', function(done){
-    
+
     var models = modeler( [
       animals_db,
       objects_db
     ])
-    
-    
+
+
     done()
   })
-  
+
   it('should load models', function(done){
-    
+
     var models = modeler( [
       animals_db,
       objects_db
@@ -48,32 +48,32 @@ describe('Modeler', function(){
         require('./schemas/dogs'),
         require('./schemas/toys')
       ], true)
-    
+
       models.definitions
       .should.have.keys(
-        'animals.Cats', 
+        'animals.Cats',
         'animals.Dogs',
         'objects.Toys'
       )
-    
+
     done()
-    
+
   })
-  
+
   it('should return Cats model', function(done){
-    
+
     var models = modeler( [
       animals_db],[
         require('./schemas/cats')
       ], true);
-    
+
       models.find('Cats', 'animals').should.be.ok;
       done()
   })
-  
-  
+
+
   it('should load Dogs and Toys model on demand', function(done){
-    
+
     var models = modeler( [
       animals_db,
       objects_db
@@ -127,11 +127,18 @@ describe('Modeler', function(){
         require('./schemas/dogs'),
         require('./schemas/toys')
       ]);
-      
+
       models.require('Dogs',  'animals').should.be.ok;
       models.find('Toys',  'objects').should.be.ok;
-      
+
       done()
   })
-  
+
+  it('should add new database connection', function(done){
+    var models = modeler();
+    models.connect( [ objects_db]);
+    models.dbs.should.have.ownProperty('objects');
+    done()
+  })
+
 })
